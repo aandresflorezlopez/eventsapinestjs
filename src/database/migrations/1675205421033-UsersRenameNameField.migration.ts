03460c2ef23d9da37a11a9ae2a3f267a9368e752
Andres Florez Lopez <aandresflorezlopez@gmail.com>
2023-02-01 19:20:24 -0500

    migrations works well

--- src/database/migrations/1675205421033-UsersRenameNameField.migration.ts
+++ src/database/migrations/1675205421033-UsersRenameNameField.migration.ts
@@ -1,16 +1,15 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 // npx typeorm-ts-node-commonjs migration:create ./src/database/migrations/UsersRenameNameField
+// npx typeorm-ts-node-commonjs migration:run -d ./ormconfig.ts
+// npx typeorm-ts-node-commonjs migration:revert -d ./ormconfig.ts
 
 export class UsersRenameNameField1675205421033 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "users" RENAME COLUMN "name" TO "title"`,
-    );
+    // await queryRunner.query(`ALTER TABLE users CHANGE name title varchar(255)`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "users" RENAME COLUMN "title" TO "name"`,
-    ); // reverts things made in "up" method
+    // reverts things made in "up" method
+    // await queryRunner.query(`ALTER TABLE users CHANGE title name varchar(255)`);
   }
 }
